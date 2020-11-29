a30e3bbabfc140f0cd11fcdd9f7df063bb820e47
mlatka <mlatka@gmail.com>
2020-11-30 00:25:12 +0100

    Branch creation

--- src/edu/ib/Main.java
+++ src/edu/ib/Main.java
@@ -3,6 +3,6 @@ package edu.ib;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello world - Mirek!");
     }
 }
